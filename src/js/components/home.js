89fefe437e9c00ebe7f2f051181c1451099b3759
James <triskept@gmail.com>
2020-01-26 14:46:04 -0500

    started first section, image needs to sized properly and a better font is needed

--- src/js/components/home.js
+++ src/js/components/home.js
@@ -1,8 +1,10 @@
 export default function Home() {
     return `
-    <div id="home">
-        <h2>Release</h2>
-        <h5>An easy way to clear a cluttered mind</h5>
-    </div>
+    <section id="home" class="row">
+        <div id="homediv">
+            <h2>Release</h2>
+            <h5>An easy way to clear a cluttered mind</h5>
+        </div>
+    </section>
     `
 }
