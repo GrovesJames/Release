56b038e34463f71592b5df0a482937f58387c74f
James <triskept@gmail.com>
2020-01-28 15:53:32 -0500

    added text box and submit button but final page is not finished

--- src/js/components/home.js
+++ src/js/components/home.js
@@ -2,7 +2,7 @@ export default function Home() {
     return `
     <section id="home" class="row">
         <div id="homediv">
-            <h2>Release</h2>
+            <h1>Release</h1>
             <h5>An easy way to clear a cluttered mind</h5>
             <h6 id="beginbutton">Begin</h6>
         </div>
